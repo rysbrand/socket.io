ec86b5503037f773365257ba548dcf129ebb9aa2
rysbrand <becca.leach325@gmail.com>
2026-05-01 10:57:05 -0500

    small changes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 
 const app = express();
 app.use(cors());
+//look any file in the public folder, not just one file, __dirname appends the file path, so it launches
+//no matter where you run node from
 app.use(express.static(path.join(__dirname, "public")));
 
 const server = http.createServer(app);
@@ -16,3 +18,18 @@ const io = newServer (server, {
         methods: ["GET", "POST"],
     },
 });
+
+//setting vote info
+const question = "What's your programming language? 🐻‍❄️";
+
+const options = [
+  { id: "js",     label: "JavaScript 🌸", emoji: "🌸" },
+  { id: "python", label: "Python 🍓",     emoji: "🍓" },
+  { id: "cpp",    label: "C++ 🎀",        emoji: "🎀" },
+  { id: "csharp", label: "C# 💗",         emoji: "💗" },
+];
+
+//vote mapping
+//sets them equal to zero
+let votes = Object.fromEntries(options.map((o) => [o.id,  0]));
+
